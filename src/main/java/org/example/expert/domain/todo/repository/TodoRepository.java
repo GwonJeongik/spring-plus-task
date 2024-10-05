cfba721c8b314fb32555334190f7e6cddc617a80
ik <jeongik11029@naver.com>
2024-10-06 03:10:41 +0900

    [update] : 필수 구현 기능 - 5. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,15 +7,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    /* weather, 수정일 기준 검색 JPQL*/
+    @Query("""
+            select t from Todo t
+            LEFT JOIN FETCH  t.user u
+            where :weather is null or t.weather = :weather
+            and :start is null or (t.modifiedAt >= :start)
+            and :end is null or (t.modifiedAt <= :end)
+            ORDER BY t.modifiedAt DESC
+            """)
+    Page<Todo> findAllByOrderByModifiedAtDesc(
+            @Param(value = "weather") String weather,
+            @Param(value = "start") LocalDateTime start,
+            @Param(value = "end") LocalDateTime end,
+            Pageable pageable
+    );
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
 }
